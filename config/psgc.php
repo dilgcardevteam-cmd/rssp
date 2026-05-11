e418f5bb7d3e9d1db14c6bf56fc02cff7e331bc1
billygitaccount2025 <dilgcardevteam@gmail.com>
2026-05-11 17:19:47 +0800

    citymun/province

---
+++ config/psgc.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'data_file' => env('PSGC_DATA_FILE', 'PSGC-4Q-2025-Publication-Datafile.xlsx'),
+    'data_sheet' => env('PSGC_DATA_SHEET', 'PSGC'),
+    'provinces_file' => env('PSGC_PROVINCES_FILE', 'PSGC_Provinces.xlsx'),
+    'provinces_sheet' => env('PSGC_PROVINCES_SHEET', 'Provinces'),
+    'cities_file' => env('PSGC_CITIES_FILE', 'PSGC_City_Municipality.xlsx'),
+    'cities_sheet' => env('PSGC_CITIES_SHEET', 'City-Municipality'),
+];
